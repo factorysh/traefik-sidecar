2062bdb9db1a1e9af9129f703f87121e60691e0d
Mathieu Lecarme <mlecarme@bearstech.com>
2020-06-26 18:04:41 +0200

    Cobra rulez.

---
+++ cmd/watch.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/docker/docker/client"
+	"github.com/factorysh/docker-visitor/visitor"
+	"github.com/factorysh/traefik-sidecar/events"
+	"github.com/factorysh/traefik-sidecar/projects"
+	"github.com/factorysh/traefik-sidecar/web"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var (
+	traefikHost     string
+	traefikPassword string
+)
+
+func init() {
+	rootCmd.AddCommand(watchCmd)
+	watchCmd.PersistentFlags().StringVarP(&traefikHost, "traefik", "t", "http://localhost:8080", "Træfik admin url")
+	watchCmd.PersistentFlags().StringVarP(&traefikPassword, "password", "", "", "Træfik admin password")
+}
+
+var watchCmd = &cobra.Command{
+	Use:   "watch",
+	Short: "watch for events",
+	Long:  ``,
+	RunE:  watch,
+}
+
+func watch(cmd *cobra.Command, args []string) error {
+	docker, err := client.NewEnvClient()
+	if err != nil {
+		return err
+	}
+	w := visitor.New(docker)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go func() {
+		err := w.Start(ctx)
+		if err != nil {
+			log.WithError(err).Error()
+		}
+	}()
+	log.Info("Listen docker events")
+	p := projects.New(w)
+	c, err := events.New(traefikHost, "admin", traefikPassword, p)
+	if err != nil {
+		return err
+	}
+	mux := http.NewServeMux()
+	ctx2 := context.Background()
+	mux.Handle("/events", web.New(ctx2, c.Events))
+	go c.WatchBackends()
+	log.Info("watch traefik's backends")
+	log.Info("Listening HTTP")
+	err = http.ListenAndServe(":3000", mux)
+	if err != nil {
+		log.WithError(err).Error()
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
 import (
-	"context"
-	"net/http"
-	"os"
-
-	"github.com/docker/docker/client"
-	"github.com/factorysh/docker-visitor/visitor"
-	"github.com/factorysh/traefik-sidecar/events"
-	"github.com/factorysh/traefik-sidecar/projects"
-	"github.com/factorysh/traefik-sidecar/web"
+	"github.com/factorysh/traefik-sidecar/cmd"
 	"github.com/onrik/logrus/filename"
 	log "github.com/sirupsen/logrus"
 )
@@ -18,35 +10,5 @@ func main() {
 	filenameHook := filename.NewHook()
 	log.AddHook(filenameHook)
 	log.SetLevel(log.DebugLevel)
-	docker, err := client.NewEnvClient()
-	if err != nil {
-		panic(err)
-	}
-	w := visitor.New(docker)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	go func() {
-		err := w.Start(ctx)
-		if err != nil {
-			log.WithError(err).Error()
-		}
-	}()
-
-	log.Info("Listen docker events")
-	p := projects.New(w)
-	c, err := events.New("http://localhost:8080", "admin", os.Getenv("PASSWORD"), p)
-	if err != nil {
-		panic(err)
-	}
-	mux := http.NewServeMux()
-	ctx2 := context.Background()
-	mux.Handle("/events", web.New(ctx2, c.Events))
-	go c.WatchBackends()
-	log.Info("watch traefik's backends")
-	log.Info("Listening HTTP")
-	err = http.ListenAndServe(":3000", mux)
-	if err != nil {
-		log.WithError(err).Error()
-	}
-
+	cmd.Execute()
 }
